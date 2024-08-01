0d78c9bbe9028960df1fd549a53d958b6b14e4a3
Egor Godovanets <godovanishe@gmail.com>
2024-08-01 11:40:59 +0300

    fix

--- src/app/constants/constants.const.ts
+++ src/app/constants/constants.const.ts
@@ -1,5 +1,5 @@
 export const apiUrl = 'https://v6.exchangerate-api.com/v6/<API_KEY>/latest/<BASE_CURRENCY>';
 
-export const apiKey = '8f9222fc57c94f7a317d799d';
+export const apiKey = '20cd475f28f185d84905a94d';
 
 export const currencies = ['UAH', 'USD', 'EUR', 'CHF', 'PLN'];
